b37eeb19670945932777b3e38df2c0912c73f211
Romain <72873530+romainvidon@users.noreply.github.com>
2021-03-09 16:08:56 +0100

    f_message_erreur: message pour l'âge

--- src/app/fininscription/fininscription.module.ts
+++ src/app/fininscription/fininscription.module.ts
@@ -8,6 +8,7 @@ import { FininscriptionPageRoutingModule } from './fininscription-routing.module
 
 import { FininscriptionPage } from './fininscription.page';
 import { PatternHeaderModule } from '../pattern-header/pattern-header.module';
+import { ErrorModule } from '../error/error.module';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { PatternHeaderModule } from '../pattern-header/pattern-header.module';
     
     PatternHeaderModule,
     FininscriptionPageRoutingModule,
-    PatternHeaderModule
+    PatternHeaderModule,
+    ErrorModule,
   ],
 
   declarations: [FininscriptionPage]
