03ef44f89df80320a9317b1f9103979e4370fbfe
Romain <72873530+romainvidon@users.noreply.github.com>
2021-03-11 15:03:03 +0100

    f_page_compte: création page Mon Compte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'compte',
+    loadChildren: () => import('./compte/compte.module').then( m => m.ComptePageModule)
   }
 
 
