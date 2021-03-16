abb1c811ea16b3c6da290d01f5d0d3f5399797ca
RubenVBarros <rubenvenancio67@gmail.com>
2021-03-16 20:37:56 +0100

    f_modifprofil: redesign de la vue et autres choses

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -7,10 +7,45 @@ import { User } from '../user';
   styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent implements OnInit {
-  @Input() editable: boolean;
+  @Input() editable_image: boolean = false;
+  @Input() editable_age: boolean = false;
+  @Input() editable_bio: boolean = false;
   @Input() user: User;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+      console.log(this.editable_image,this.editable_age,this.editable_bio);
+  }
+
+  activateAge(){
+      if(this.editable_age == false){
+        this.editable_age = true;
+      }
+      else{
+        this.editable_age = false;
+
+      }
+      console.log(this.editable_age);
+  }
+  activateBio(){
+    if(this.editable_bio == false){
+      this.editable_bio = true;
+    }
+    else{
+      this.editable_bio = false;
+
+    }
+    console.log(this.editable_bio);
+}
+activateImage(){
+    if(this.editable_image == false){
+      this.editable_image = true;
+    }
+    else{
+      this.editable_image = false;
+
+    }
+    console.log(this.editable_image);
+}
 
 }
