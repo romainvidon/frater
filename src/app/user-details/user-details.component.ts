2f2597cf42411739ab898dfd5ac001fd41dffafc
RubenVBarros <rubenvenancio67@gmail.com>
2021-03-17 17:59:30 +0100

    f_modifprofil

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../user';
+import { UserService } from '../user.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-user-details',
@@ -8,44 +10,75 @@ import { User } from '../user';
 })
 export class UserDetailsComponent implements OnInit {
   @Input() editable_image: boolean = false;
-  @Input() editable_age: boolean = false;
+  @Input() editable_pseudo: boolean = false;
   @Input() editable_bio: boolean = false;
   @Input() user: User;
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit() {
-      console.log(this.editable_image,this.editable_age,this.editable_bio);
+      console.log(this.editable_image,this.editable_pseudo,this.editable_bio);
+      this.user = this.userService.blank();
+      this.authService.getCurrentUser().then((result)=>{
+      if(result.isOk){
+        this.user = result.user;
+        }
+        this.user.bio ??= "Aucun contenu pour le moment. Éditez votre bio.";
+      });
   }
 
-  activateAge(){
-      if(this.editable_age == false){
-        this.editable_age = true;
-      }
-      else{
-        this.editable_age = false;
-
-      }
-      console.log(this.editable_age);
-  }
-  activateBio(){
-    if(this.editable_bio == false){
-      this.editable_bio = true;
+activatePseudo(){
+    if(this.editable_pseudo == false) {
+        this.editable_pseudo = true;
     }
-    else{
-      this.editable_bio = false;
-
+    else {
+        this.editable_pseudo = false;
+    }
+    console.log(this.editable_pseudo);
+  }
+activateBio(){
+    if(this.editable_bio == false) {
+        this.editable_bio = true;
+    }
+    else {
+        this.editable_bio = false;
     }
     console.log(this.editable_bio);
-}
+  }
 activateImage(){
-    if(this.editable_image == false){
-      this.editable_image = true;
+    if(this.editable_image == false) {
+        this.editable_image = true;
     }
-    else{
-      this.editable_image = false;
-
+    else {
+        this.editable_image = false;
     }
     console.log(this.editable_image);
-}
+  }
+deactivateBio(){
+    if(this.editable_bio == true) {
+          this.editable_bio = false;
+    }
+      else{
+          this.editable_bio = true;
+    }
+    console.log(this.editable_bio);
+  }
+deactivatePseudo(){
+    if(this.editable_pseudo == true) {
+          this.editable_pseudo = false;
+    }
+      else {
+          this.editable_pseudo = true;
+    }
+    console.log(this.editable_pseudo);
+  }
+  changeBio(item:any){
+    console.log(item.target.value);
+    this.authService.patchCurrentUser({bio:item.target.value});
+  }
+  changePseudo(item:any){
+    console.log(item.target.value);
+    this.authService.patchCurrentUser({pseudo:item.target.value});
+  }
 
 }
+
